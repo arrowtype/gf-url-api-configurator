64fc9353207a6e18180ea8b3e1e797f92e161d57
Stephen Nixon <stephen@thundernixon.com>
2019-12-12 18:16:24 -0500

    update API call for range or pinned wght selection

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,23 @@
 // import list from './modules/list'
 // console.log(`Hello ${list[2]}`)
 
-
+let selectedSources = [300, 1000]
 let wght = 400
 let MONO = 0
 
 function setUrl() {  
-  wght = wght_pinned_slider.value
   MONO = MONO_pinned_slider.value
+
+  let result = `@import url('https://fonts.sandbox.google.com/css2?family=Recursive:wght,MONO@300..1000,0');`
   
-  let result = `@import url('https://fonts.sandbox.google.com/css2?family=Recursive:wght,MONO@${wght},${MONO}');`
+  if (wghtSubsetControls.dataset.subsetType === "range") {
+    console.log(selectedSources)
+    result = `@import url('https://fonts.sandbox.google.com/css2?family=Recursive:wght,MONO@${selectedSources[0]}..${selectedSources[1]},${MONO}');`
+  } else {
+    wght = wght_pinned_slider.value
+    result = `@import url('https://fonts.sandbox.google.com/css2?family=Recursive:wght,MONO@${wght},${MONO}');`
+  }
+  
   
   api_call.innerHTML = result
 }
@@ -18,11 +26,11 @@ function setUrl() {
 
 const wghtSubsetControls = document.querySelector('#wght-control .subset-controls')
 
-document.getElementById("wght_subset__range").addEventListener('input', (e) => {  
+document.getElementById("wght_subset__range").addEventListener('input', () => {  
   wghtSubsetControls.dataset.subsetType = "range"
 });
 
-document.getElementById("wght_subset__pinned").addEventListener('input', (e) => {
+document.getElementById("wght_subset__pinned").addEventListener('input', () => {
   wghtSubsetControls.dataset.subsetType = "pinned"
 });
 
@@ -68,47 +76,39 @@ function selectRange(e) {
     console.log("click")
     console.log("closest is ", e.target.closest('#wght__range'))
     // if user clicks outside of controller, reset to previous state
-    // if (e.target.closest('#wght__range') === null) {
     if (e.target.matches('.range-selector') === false) {
       console.log("click outside!")
       resetControls()
-      } 
+      }
+    // if user clicks the same selector a second time
     else if (e.target.dataset.selected === "true") {
       console.log("click on same selector!")
       resetControls()
     }
-    else 
-      {
+    // if user clicks a different selector 
+    else {
       console.log("click on selector!")
       e.target.dataset.selected = "true"
       rangeSelectorControl.classList.remove("active")
       document.removeEventListener('mousedown',nextClick)
     }
-    // update URL with selected elements
-
+    
+    selectedSources = []
+    // check what sources are selected
+    for (var selector of rangeSelectorNodes) {if (selector.dataset.selected === "true") {selectedSources.push(parseInt(selector.dataset.wghtSrc))}}
     
     // if 300 and 1000 are selected, also set 800 to "true"
-    let selectedSources = []
-    for (var selector of rangeSelectorNodes) {
-      if (selector.dataset.selected === "true") {
-        selectedSources.push(selector.dataset.wghtSrc)
-      }
-    }
-    if (selectedSources.includes("300") && selectedSources.includes("1000")) {
+    if (selectedSources.includes(300) && selectedSources.includes(1000)) {
       document.querySelector('[data-wght-src="800"]').dataset.selected = "true"
     }
 
-    console.log(selectedSources)
-
+    setUrl()
 
     // re-add listeners after delay, or the setting won't stick
     window.setTimeout(function() {
       console.log("adding listeners")
       addRangeListeners();
     }, 500);
-    
-
-    
   })
 
 }
@@ -120,13 +120,6 @@ function addRangeListeners() {
 }
 
 addRangeListeners()
-
-// UX QUESTION: do calendar range selections only work well because they must be opened, which makes it clear that a range should be selected?
-
-
-
-
-
 
 
 // -----------------------------------------------------------------------------
